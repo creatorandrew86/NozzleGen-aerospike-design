87d251169c181bc99cfea2d222d172524c79f75c
creatorandrew86 <btoma0513@gmail.com>
2026-07-22 20:36:09 +0300

    removed faulty icon

--- Source Code/main.py
+++ Source Code/main.py
@@ -1,16 +1,6 @@
 import tkinter as tk
-import sys, os
 
 import geometry, interface, input
-
-def _resource_path(relative_path):
-    try:
-        base_path = sys._MEIPASS
-    except AttributeError:
-        base_path = os.path.abspath(".")
-
-    return os.path.join(base_path, relative_path)
-
 
 def on_solve():
     inputs = ui.get_inputs_on_solve()
@@ -41,9 +31,6 @@ if __name__ == "__main__":
     root.title('Aerospike')
     root.geometry('570x330')
     root.resizable(False, False)
-
-    icon_path = _resource_path(os.path.join("assets", "icon.ico"))
-    root.iconbitmap(icon_path)
 
     ui = interface.Interface(root=root, on_solve=on_solve, on_save=on_save, results=None, errors=None)
 
